e46c354344750b4a947e0a6fefa4a1695d1ad160
Pauloxhbh <phas3@aluno.ifal.edu.br>
2024-04-01 09:26:15 -0300

    fase inicial do aplicativo

---
+++ gato.js
@@ -0,0 +1,12 @@
+import { View, Text} from 'react-native';
+
+const Gato = () => {
+    return(
+    <View>
+        <Text> Meu nome é fulano e o dele é ciclano </Text>
+    </View>
+    );
+}
+
+
+export default Gato;
