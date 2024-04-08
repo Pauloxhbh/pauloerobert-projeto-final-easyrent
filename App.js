fac735b6b2597ffa3842dbaded5ff1c5519f2adc
Pauloxhbh <phas3@aluno.ifal.edu.br>
2024-04-08 08:27:48 -0300

    easyrent projeto final

--- App.js
+++ App.js
@@ -4,13 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Text>'bom dia'</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { 
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
